02df1aa09ebe69d303fb655425617a2d822eb0b1
Brix Secretaria <secretaria1998@yahoo.com>
2017-12-06 07:19:13 +0000

    Edit app.js

--- app.js
+++ app.js
@@ -3,47 +3,116 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
-
-var mongoose = require('mongoose');
-var config = require('./config');
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+var watson = require('watson-developer-cloud');
 var app = express();
-//hehe
-var setupController = require('./controllers/setupController');
-var apiController = require('./controllers/apiController');
+var contexid = "";
 
-//var port = process.env.PORT || 3000;
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-mongoose.connect(config.getDbConnectionString(), {
-	useMongoClient:true
+var conversation_id = "";
+var w_conversation = watson.conversation({
+    url: 'https://gateway.watsonplatform.net/conversation/api',
+    username: process.env.CONVERSATION_USERNAME || 'paste conversation api username here',
+    password: process.env.CONVERSATION_PASSWORD || 'paste conversation api password here',
+    version: 'v1',
+    version_date: '2016-07-11'
 });
-setupController(app);
-apiController(app);
-//random comment
-// all environments
-app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+var workspace = process.env.WORKSPACE_ID || 'workspaceId';
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'paste FB token here') {
+        res.send(req.query['hub.challenge']);
+    }
+    res.send('Erro de validação no token.');
+});
+
+app.post('/webhook/', function (req, res) {
+	var text = null;
+	
+    messaging_events = req.body.entry[0].messaging;
+	for (i = 0; i < messaging_events.length; i++) {	
+        event = req.body.entry[0].messaging[i];
+        sender = event.sender.id;
+
+        if (event.message && event.message.text) {
+			text = event.message.text;
+		}else if (event.postback && !text) {
+			text = event.postback.payload;
+		}else{
+			break;
+		}
+		
+		var params = {
+			input: text,
+			// context: {"conversation_id": conversation_id}
+			context:contexid
+		}
+
+		var payload = {
+			workspace_id: "paste workspace ID here"
+		};
+
+		if (params) {
+			if (params.input) {
+				params.input = params.input.replace("\n","");
+				payload.input = { "text": params.input };
+			}
+			if (params.context) {
+				payload.context = params.context;
+			}
+		}
+		callWatson(payload, sender);
+    }
+    res.sendStatus(200);
+});
+
+function callWatson(payload, sender) {
+	w_conversation.message(payload, function (err, convResults) {
+		 console.log(convResults);
+		contexid = convResults.context;
+		
+        if (err) {
+            return responseToRequest.send("Erro.");
+        }
+		
+		if(convResults.context != null)
+    	   conversation_id = convResults.context.conversation_id;
+        if(convResults != null && convResults.output != null){
+			var i = 0;
+			while(i < convResults.output.text.length){
+				sendMessage(sender, convResults.output.text[i++]);
+			}
+		}
+            
+    });
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
-//app.listen(port);
+function sendMessage(sender, text_) {
+	text_ = text_.substring(0, 319);
+	messageData = {	text: text_ };
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: { access_token: token },
+        method: 'POST',
+        json: {
+            recipient: { id: sender },
+            message: messageData,
+        }
+    }, function (error, response, body) {
+        if (error) {
+            console.log('Error sending message: ', error);
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error);
+        }
+    });
+};
+
+var token = "paste FB token here";
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+var port = (process.env.VCAP_APP_PORT || 3000);
+app.listen(port, host);

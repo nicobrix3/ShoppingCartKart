0d9b582cb9806867ab06e6685061eed2a0f199f5
Brix Nicholson Secretaria <secretaria1998@yahoo.com>
2017-12-11 19:36:59 +0800

    zzz

--- app.js
+++ app.js
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 require('dotenv').load();
 
 var middleware = require('botkit-middleware-watson')({

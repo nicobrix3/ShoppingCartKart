ce5e2cfdaefe28912d238f85849d28614cf9e197
Brix Nicholson Secretaria <secretaria1998@yahoo.com>
2017-12-12 17:04:22 +0800

    edit app.js

--- app.js
+++ app.js
@@ -45,13 +45,14 @@ module.exports = function(app) {
   // Customize your Watson Middleware object's before and after callbacks.
   middleware.before = function(message, conversationPayload, callback) {
 	  //Passing values to conversation.
-	  console.log('Inside the before method.  messageB=' + JSON.stringify(message, 2, null));
-	  //callback(null, conversationPayload);
+	  console.log('Inside the before method. message=' + JSON.stringify(message));
+	  //console.log('Inside the before method.  messageB=' + JSON.stringify(message, 2, null));
+	  callback(null, conversationPayload);
 	  }
 
   middleware.after = function(message, conversationResponse, callback) {
-	  console.log("Attempting to respond");
+	    console.log("Attempting to respond");
                  // *** Call to remote service here ***
-	    //callback(null, conversationResponse);
+	    callback(null, conversationResponse);
   	  }
 };

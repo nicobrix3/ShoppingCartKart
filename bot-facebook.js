44cf8dfd0baec8a41f051c8b7b422ced4180d208
Brix Nicholson Secretaria <secretaria1998@yahoo.com>
2017-12-14 20:58:28 +0800

    try icon_emoji

--- bot-facebook.js
+++ bot-facebook.js
@@ -37,12 +37,65 @@ bot.say('Hello Fellow!');
 
 controller.hears('goodbyes', 'message_received', middleware.hear, function(bot,message) {
 	/*console.log("Goodbye Intent Identified");
-	return bot.reply(message, "Bye! K.");*/
+	return bot.reply(message, "Bye! K.");*/ //regular response
 	return bot.reply(message,{
 	      text: "A more complex response",
-	      username: "ReplyBot",
-	      icon_emoji: ":dash:",
-	    });
+	      icon_emoji: ":grin:"
+	    }); //slightly complex response
+	/* bot.reply(message, {
+	        attachment: {
+	            'type':'template',
+	            'payload':{
+	                 'template_type':'generic',
+	                 'elements':[
+	                   {
+	                     'title':'Classic White T-Shirt',
+	                     'image_url':'http://petersapparel.parseapp.com/img/item100-thumb.png',
+	                     'subtitle':'Soft white cotton t-shirt is back in style',
+	                     'buttons':[
+	                       {
+	                         'type':'web_url',
+	                         'url':'https://petersapparel.parseapp.com/view_item?item_id=100',
+	                         'title':'View Item'
+	                       },
+	                       {
+	                         'type':'web_url',
+	                         'url':'https://petersapparel.parseapp.com/buy_item?item_id=100',
+	                         'title':'Buy Item'
+	                       },
+	                       {
+	                         'type':'postback',
+	                         'title':'Bookmark Item',
+	                         'payload':'USER_DEFINED_PAYLOAD_FOR_ITEM100'
+	                       }
+	                     ]
+	                   },
+	                   {
+	                     'title':'Classic Grey T-Shirt',
+	                     'image_url':'http://petersapparel.parseapp.com/img/item101-thumb.png',
+	                     'subtitle':'Soft gray cotton t-shirt is back in style',
+	                     'buttons':[
+	                       {
+	                         'type':'web_url',
+	                         'url':'https://petersapparel.parseapp.com/view_item?item_id=101',
+	                         'title':'View Item'
+	                       },
+	                       {
+	                         'type':'web_url',
+	                         'url':'https://petersapparel.parseapp.com/buy_item?item_id=101',
+	                         'title':'Buy Item'
+	                       },
+	                       {
+	                         'type':'postback',
+	                         'title':'Bookmark Item',
+	                         'payload':'USER_DEFINED_PAYLOAD_FOR_ITEM101'
+	                       }
+	                     ]
+	                   }
+	                 ]
+	               }
+	        }
+	    });*/
 });
 
 controller.hears('(.*)', 'message_received', function(bot, message) {

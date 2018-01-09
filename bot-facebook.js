444ee5542b0fb62a4d18ff74af15be76b53ea431
Brix Secretaria <secretaria2001@yahoo.com>
2018-01-09 15:20:03 +0800

    please work mongo storage

--- bot-facebook.js
+++ bot-facebook.js
@@ -15,13 +15,20 @@
  */
 
 var Botkit = require('botkit');
+var mongoStorage = require('./brix_dep/botkit-storage-mongo')({mongoUri:'mongodb://Marponsie:Password8732!@ds147882.mlab.com:47882/boiband'});
 
 var controller = Botkit.facebookbot({
   access_token: process.env.FB_ACCESS_TOKEN,
-  verify_token: process.env.FB_VERIFY_TOKEN
+  verify_token: process.env.FB_VERIFY_TOKEN,
+  storage: mongoStorage
 });
 
 var bot = controller.spawn();
+
+controller.storage.users.get('11111', function(error, beans){
+  console.log(beans);
+});
+
 controller.hears('(.*)', 'message_received', function(bot, message) {
   bot.reply(message, message.watsonData.output.text.join('\n'));
 });

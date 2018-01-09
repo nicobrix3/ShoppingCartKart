94bb6560a5fdef3a43318321e86a1ed7664deca6
Brix Secretaria <secretaria2001@yahoo.com>
2018-01-09 15:53:06 +0800

    Storage Storage Storage

--- bot-facebook.js
+++ bot-facebook.js
@@ -15,12 +15,12 @@
  */
 
 var Botkit = require('botkit');
-//var mongoStorage = require('./brix_dep/botkit-storage-mongo')({mongoUri:'mongodb://Marponsie:Password8732!@ds147882.mlab.com:47882/boiband'});
+var mongoStorage = require('./brix_dep/botkit-storage-mongo')({mongoUri:'mongodb://Marponsie:Password8732!@ds147882.mlab.com:47882/boiband'});
 //var mongoStorage = require('botkit-storage-mongo')({mongoUri:'mongodb://Marponsie:Password8732!@ds147882.mlab.com:47882/boiband'}); 
 var controller = Botkit.facebookbot({
   access_token: process.env.FB_ACCESS_TOKEN,
   verify_token: process.env.FB_VERIFY_TOKEN,
-  //storage: mongoStorage
+  storage: mongoStorage
 });
 
 var bot = controller.spawn();

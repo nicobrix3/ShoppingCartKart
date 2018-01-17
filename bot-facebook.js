fc45f579bf5b45af77b37400a634ebb438743171
Brix Secretaria <secretaria2001@yahoo.com>
2018-01-17 14:24:12 +0800

    try storage in controller

--- bot-facebook.js
+++ bot-facebook.js
@@ -16,10 +16,12 @@
 
 var Botkit = require('botkit');
 var clone = require('clone');
+var mongoStorage = require('./brix_dep/botkit-storage-mongo')({mongoUri:'mongodb://Marponsie:Password8732!@ds147882.mlab.com:47882/boiband', tables: ['userdata']});
 
 var controller = Botkit.facebookbot({
   access_token: process.env.FB_ACCESS_TOKEN,
   verify_token: process.env.FB_VERIFY_TOKEN,
+  storage: mongoStorage
 });
 
 var bot = controller.spawn();

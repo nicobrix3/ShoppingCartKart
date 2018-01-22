ccfa612cf9eff8b2fcfa90ccfcad9eaf980d6190
Brix Secretaria <secretaria2001@yahoo.com>
2018-01-22 19:05:14 +0800

    plez work JSON.Stringify()

--- app.js
+++ app.js
@@ -61,22 +61,22 @@ module.exports = function(app) {
     console.log('Twilio bot is live');
   }
 
-  storage.channels.get('1772861762745413', function(error, beans){
+  storage.channels.get(1772861762745413, function(error, beans){
     username = beans.contextVar.user_name;
     shoeBrand = beans.contextVar.shoe_brand;
     shoeType = beans.contextVar.shoe_type;
     shoeColor = beans.contextVar.shoe_color;
   });
  
-  /*console.log("user_name: " + JSON.stringify(username));
+  console.log("user_name: " + JSON.stringify(username));
   console.log("shoe_brand: " + JSON.stringify(shoe_brand));
   console.log("shoe_type: " + JSON.stringify(shoeType));
-  console.log("shoe_color: " + JSON.stringify(shoeColor));*/
+  console.log("shoe_color: " + JSON.stringify(shoeColor));
 
-  console.log("user_name: " + username);
+  /*console.log("user_name: " + username);
   console.log("shoe_brand: " + shoeBrand);
   console.log("shoe_type: " + shoeType);
-  console.log("shoe_color: " + shoeColor);
+  console.log("shoe_color: " + shoeColor);*/
 
   // Customize your Watson Middleware object's before and after callbacks.
   middleware.before = function(message, conversationPayload, callback) {

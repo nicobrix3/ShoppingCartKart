c67d770a100bacf0de1f673d7038e64a59785f72
Brix Secretaria <secretaria2001@yahoo.com>
2018-01-23 15:39:38 +0800

    no more time out bug plez

--- app.js
+++ app.js
@@ -137,16 +137,16 @@ module.exports = function(app) {
 
     var lastActivityTime = new Date();
     console.log("Date: " + JSON.stringify(lastActivityTime));
+    storage.channels.save({id: message.channel, date: lastActivityTime, contextVar: conversationResponse.context}, function(err) {
+      if(err){
+        console.log("Warning: error saving channel details: " + JSON.stringify(err));
+      }
+      else{
+        console.log("Success saving channel detail.");
+      }
+    });
     if(typeof conversationResponse !== 'undefined' && typeof conversationResponse.output !== 'undefined'){
       if(conversationResponse.output.action === 'save_full_record'){
-        storage.channels.save({id: message.channel, date: lastActivityTime, contextVar: conversationResponse.context}, function(err) {
-          if(err){
-            console.log("Warning: error saving channel details: " + JSON.stringify(err));
-          }
-          else{
-            console.log("Success saving channel detail.");
-          }
-        });
         storage.channels.get(message.channel, function(error, beans){
           username = beans.contextVar.user_name;
           shoeBrand = beans.contextVar.shoe_brand;
